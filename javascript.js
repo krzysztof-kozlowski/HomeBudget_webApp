577e2c420e115691a9dc16b0a95307bf858b1cab
Krzysztof <k.kozlowski.tychy@gmail.com>
2018-11-23 23:31:01 +0100

    Styled sub-menu where user can choose the period of money report

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+/*$(document).ready(function () {
   var date_input = $('input[name="date"]'); //our date input has the name "date"
   var container = $('.bootstrap-iso form').length > 0 ? $('.bootstrap-iso form').parent() : "body";
   date_input.datepicker({
@@ -7,4 +7,26 @@ $(document).ready(function () {
       todayHighlight: true,
       autoclose: true,
   })
-})
+})*/
+
+// Make your own here: https://eternicode.github.io/bootstrap-datepicker
+$(document).ready(function () {
+  var dateSelect = $('#period-datepicker');
+  var startDate = $('#start-date');
+  var endDate = $('#end-date');
+  var spanStart = $('.date-depart');
+  var spanEnd = $('.date-return');
+  var spanDateFormat = 'YYYY, MM, DD';
+
+  dateSelect.datepicker({
+    autoclose: true,
+    format: "yyyy/mm/dd",
+    maxViewMode: 0,
+    startDate: "now"
+  }).on('change', function () {
+    var start = $.format.date(startDate.datepicker('getDate'), spanDateFormat);
+    var end = $.format.date(endDate.datepicker('getDate'), spanDateFormat);
+    spanStart.text(start);
+    spanEnd.text(end);
+  })
+});
